3766ee3c543496a3d1a0c05b34b98ea326e62c00
Gyusun Yeom <omniavinco@gmail.com>
2014-11-12 23:02:57 +0900

    Add GTK project

---
+++ Quack/Quack.GTK/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+using Quack;
+
+public partial class MainWindow : Gtk.Window, IPinAuthHandler
+{
+    public MainWindow()
+        : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
---
+++ Quack/Quack.GTK/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace Quack.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            win.Title = "Quaks";
+            Application.Run();
+        }
+    }
+}
