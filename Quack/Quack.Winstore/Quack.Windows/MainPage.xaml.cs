1a94c2e9452a292bc957090c40abd19b5c55962b
Gyusun Yeom <omniavinco@gmail.com>
2014-11-12 22:33:01 +0900

    reorganize projects - use shared project - fix windows project - add disabled winphone project(for future?)

--- Quack/Quack.Winstore/Quack.Windows/MainPage.xaml.cs
+++ Quack/Quack.Winstore/Quack.Windows/MainPage.xaml.cs
@@ -13,12 +13,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// 빈 페이지 항목 템플릿에 대한 설명은 http://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
 
 namespace Quack
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// 자체에서 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
     /// </summary>
     public sealed partial class MainPage : Page
     {
