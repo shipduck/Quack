ab2ba4f9900477ff10b1776ffdd6ee54ce966653
Gyusun Yeom <omniavinco@gmail.com>
2015-05-04 10:00:24 +0900

    Add android project

---
+++ Quack/Quack.android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Quack.android
+{
+	[Activity (Label = "Quack", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			base.RequestWindowFeature (WindowFeatures.NoTitle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			Button writeButton = FindViewById<Button> (Resource.Id.write_button);
+			writeButton.Click += delegate {
+				
+			};
+		}
+
+
+	}
+}
+
+
